91a4101f75417d835d552f70b4ac48b7101d0e8c
panfiloow <panfiloow@mail.ru>
2025-11-23 18:01:18 +0300

    update: time middleware for test exec time

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 from sqlalchemy import text
 from app.db.session import engine
 from app.api.v1 import api_router
@@ -21,6 +22,19 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix="/api/v1")
+
+
+# Время выполнения запроса
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+
+    return response
 
 
 @app.get("/")
