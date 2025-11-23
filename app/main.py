f64a517611c63704b87638cc70f36752e08db882
panfiloow <panfiloow@mail.ru>
2025-11-24 00:57:00 +0300

    delete: lifespan

--- app/main.py
+++ app/main.py
@@ -1,33 +1,16 @@
-from contextlib import asynccontextmanager
 import time
 from fastapi import FastAPI, Request
 from redis import RedisError
-from sqlalchemy import text
 from app.core.exception_handlers import (
     db_exception_handler,
     generic_exception_handler,
     redis_exception_handler,
 )
-from app.db.session import engine
 from app.api.v1 import api_router
 from sqlalchemy.exc import SQLAlchemyError
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    try:
-        async with engine.begin() as conn:
-            await conn.execute(text("SELECT 1"))
-        print("✅ Connection to PostgreSQL successful!")
-    except Exception as e:
-        print(f"❌ Connection failed: {e}")
-
-    yield
-
-    await engine.dispose()
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(api_router, prefix="/api/v1")
 app.add_exception_handler(SQLAlchemyError, db_exception_handler)
 app.add_exception_handler(RedisError, redis_exception_handler)
